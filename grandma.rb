94d998c631f308bb9cde83c17735494fcccb496f
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-28 20:00:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,3 @@
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  
-  elsif phrase == "Hi Nana, How are you?" 
-  puts "NO, NOT SINCE 1938!"
-else
-  phrase == "I LOVE YOU GRANDMA!"
-  
-  end  
-end
 
 # Write a speak_to_grandma method.
 
