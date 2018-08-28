6894c6917a1971313212388074d8a425949d318c
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-28 20:01:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,6 @@
-
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKN!"
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
