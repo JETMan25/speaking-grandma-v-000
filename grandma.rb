2b1bc17d0eaa2a9160e93cd6513e3a2005bd0bdb
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-28 19:46:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,15 @@
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.
+  puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase
+  puts "NO, NOT SINCE 1938!"
+else
+  nil
+  end  
+end
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
