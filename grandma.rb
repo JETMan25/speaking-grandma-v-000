6c753d563717aee39a9e3513f8f359440c7b4768
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-28 19:59:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   
-  elsif phrase == 
+  elsif phrase == "Hi Nana, How are you?" 
   puts "NO, NOT SINCE 1938!"
 else
   phrase == "I LOVE YOU GRANDMA!"
