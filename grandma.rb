f89ea56e3fb211138c4da19a521aae24d878e7a7
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-28 20:18:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
 def speak_to_grandma(phrase)
-  if phrase.downcase
-  elsif phrase.downcase
+  if #{phrase}.downcase
   puts "HUH?! SPEAK UP SONNY!"
-  elsif phrase.upcase
+  elsif #{phrase}.upcase
   puts "NO, NOT SINCE 1938!"
 else phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKN!"
